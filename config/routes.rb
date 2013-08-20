963d366c49cfc6d08265a03fd4381babb8bcc09c
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2013-08-20 16:01:17 -0400

    add sign out link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Ericisaac::Application.routes.draw do
 
-  devise_for :admins
-
+  devise_for :admins,
+             :controllers => {
+                 :passwords => "passwords",
+                 :registrations => "registrations",
+                 :sessions => "sessions"
+             } do
+    get "sign_in", :to => "sessions#new"
+    get "sign_out", :to => "sessions#destroy"
+    get "sign_up", :to => "registrations#new"
+  end
   root :to => "welcome#index"
 
   match 'admin', :to => 'admin#index'
