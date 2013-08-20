df27fb908beb4ccf2e6d0274f9e4dc0615326972
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2013-08-20 11:37:09 -0400

    my initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ericisaac::Application.config.secret_token = 'b46ee88381372671610a2680a6c9d0a64390c055501cac1ac918c1e1cad6b07093bb79f5a00beb1e55188cf288dda76b1117633c2dc038dac9fb9a1a78965766'
