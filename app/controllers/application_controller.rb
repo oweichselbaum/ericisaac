539bb18e7c5a9cc010903565e50f90b59248ffc2
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2013-09-17 17:48:16 -0400

    add header and footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   layout 'application'
+
+  def phone_number
+    @phone = Contact.first.phone
+  end
+
+  def email_address
+    @email = Contact.first.email
+  end
 end
