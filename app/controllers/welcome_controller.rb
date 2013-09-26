14e680348471ed7a8e48e098ec6be18e1c76a116
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2013-09-26 11:49:21 -0400

    fixed weird facebook hook and hooked in metadata

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @channel = Channel.find_by_name("new work")
     @channel_photos = @channel.channel_photos.order("position DESC").all
+    @featured_photo = Photo.find(params[:featured_id]) if params[:featured_id]
   end
 
   def bio
