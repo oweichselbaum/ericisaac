4119a672d8aed58ce51feeed65bf3d40577e6763
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2013-10-24 13:34:20 -0400

    update mobile site to follow function of desktop

--- app/assets/javascripts/mobile-application-list.js
+++ app/assets/javascripts/mobile-application-list.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require pace.js
+//= require imagesloaded.js
 //= require jquery.lazyload.min
 //= require jquery.isotope.min
 //= require jquery.swipebox
--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -22,62 +22,69 @@ $(document).ready(function () {
             });
         }
     })
-    var title = window.location.pathname.replace("/", "");
-    if (title == "") {
-        $('.new-work').addClass('selected');
-    } else {
-        $('div').find('.' + title).addClass('selected');
-    }
+//    var title = window.location.pathname.replace("/", "");
+//    if (title == "") {
+//        $('.new-work').addClass('selected');
+//    } else {
+//        $('div').find('.' + title).addClass('selected');
+//    }
 
     var $container = $('#photos_container');
     var $win = $(window);
     $imgs = $("img.lazy");
-    $container.isotope({
-        itemSelector: '.photo_wrapper',
-        masonry: {
-            columnWidth: 5
-        },
-        masonryHorizontal: {
-            rowHeight: 5
-        }
+    $container.imagesLoaded(function () {
+        $container.isotope({
+            itemSelector: '.photo_wrapper',
+            masonry: {
+                columnWidth: 5
+            },
+            masonryHorizontal: {
+                rowHeight: 5
+            }
+        });
     });
 
-    $imgs.lazyload({
-        effect: "fadeIn",
-        failure_limit: Math.max($imgs.length - 1, 0)
-    });
-
-    $("img:below-the-fold").lazyload({
-        event: "sporty"
-    });
-
-    $(window).bind("load", function () {
-        setTimeout(function () {
-            $("img.lazy").trigger("sporty")
-        }, 2000);
-    });
+//    $imgs.lazyload({
+//        effect: "fadeIn",
+//        failure_limit: Math.max($imgs.length - 1, 0)
+//    });
+//
+//    $("img:below-the-fold").lazyload({
+//        event: "sporty"
+//    });
+//
+//    $(window).bind("load", function () {
+//        setTimeout(function () {
+//            $("img.lazy").trigger("sporty")
+//        }, 2000);
+//    });
 
     var swipeboxInstance = $(".swipebox-isotope:not(.isotope-hidden .swipebox-isotope)").swipebox();
 
-    onAnimationCompleted = function(){
-    	swipeboxInstance.refresh();
+    onAnimationCompleted = function () {
+        swipeboxInstance.refresh();
     };
 
 
     optionFilterLinks = $('#filter a');
 
-    $('#filters a').click( function(){
-    	var selector = $(this).attr('data-filter');
+    $('#filters a').click(function () {
+        var selector = $(this).attr('data-filter');
         $container.isotope({
-    		filter : selector,
-    		animationEngine : 'best-available'
-    	}, onAnimationCompleted); // callback here
+            filter: selector,
+            animationEngine: 'best-available'
+        }, onAnimationCompleted); // callback here
         $('#handler-right').attr('checked', false);
-    	$('.filter').removeClass('selected');
-    	$(this).addClass('selected');
+        $('.filter').removeClass('selected');
+        $(this).addClass('selected');
         setTimeout(onAnimationFinished, 600);
-    	return false;
+        return false;
     });
 
     $(".swipebox").swipebox();
+});
+
+$(window).load(function () {
+    $('.pace').delay(750).fadeOut();
+    $('.loader').delay(750).fadeOut();
 });
